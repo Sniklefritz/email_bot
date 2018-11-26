fb593d653001ac01b3dbd5aa9c27471100fb41d1
Austin <aucraig88@gmail.com>
2018-11-26 01:57:49 -0700

    Fixed email madel to import csv + downcase name and email

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,12 +1,16 @@
+require 'csv'
+require 'pry'
 class Email < ApplicationRecord
-  require 'csv'
-
   # a class method import, with file passed through as an argument
   def self.import(file)
     # a block that runs through a loop in our CSV data
     CSV.foreach(file.path, headers: true) do |row|
+      new_hash = {}
+      row.to_hash.each do |k,v|
+        new_hash.merge!({k.downcase => v.downcase})
+      end
       # creates a user for each row in the csv file
-      Email.create! row.to_hash
+      Email.create!(new_hash)
     end
   end
 end
