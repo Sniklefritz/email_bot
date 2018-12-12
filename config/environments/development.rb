7daaf780f9f75f5d5b9460c9c55e9003aea9cc80
Cameron Craig <sentrathis96@gmail.com>
2018-12-11 18:10:09 -0700

    Configure actionmailer to work with gmail / add figaro

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,6 +34,14 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 465,
+    user_name: ENV["SMTP_USERNAME"],
+    password: ENV["SMTP_PASSWORD"],
+    domain: "gmail.com",
+    authentication: :plain
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
